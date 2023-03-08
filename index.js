78ea383c5bb4f1f91355332d80f6b67e673b33cb
MDASHHADALI <127089397+MDASHHADALI@users.noreply.github.com>
2023-03-08 15:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,24 @@ function login(){
 function signUp(){
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
+    //firebase.auth().createUserWithEmailAndPassword(email, password)
+    //.catch((error) => {
+      //  document.getElementById("error").innerHTML = error.message
+            firebaseAuth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "New user registration: " + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            SignupActivity.this.showToast("Authentication failed. " + task.getException());
+                        } else {
+                            SignupActivity.this.startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                            SignupActivity.this.finish();
+                        }
+                    }
+                });
+
     });
 }
 
