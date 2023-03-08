7f6d657c6ee255f0cb5880f339b2fde12f2d141f
MDASHHADALI <127089397+MDASHHADALI@users.noreply.github.com>
2023-03-08 14:44:29 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+apiKey: "AIzaSyCZKUFr2C6W7XDS5Yv_7U94u2wHMBPO_vA",
+  authDomain: "ethereal-2cf64.firebaseapp.com",
+  databaseURL: "https://ethereal-2cf64-default-rtdb.firebaseio.com",
+  projectId: "ethereal-2cf64",
+  storageBucket: "ethereal-2cf64.appspot.com",
+  messagingSenderId: "364462287774",
+  appId: "1:364462287774:web:8d828906de3565e8cb048f",
+  measurementId: "G-JP8SLCM9WD"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
